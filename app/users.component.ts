db5caa375ac0b5dc136c15b8b088e277b7fd9747
Vinay Joshi <vinay01joshi@gmail.com>
2017-07-18 08:48:05 +0530

    Display the list of users

--- app/users.component.ts
+++ app/users.component.ts
@@ -1,11 +1,18 @@
 
-import { Component } from "angular2/core";
+import { Component, OnInit } from "angular2/core";
+import { UserService } from "./user.service";
 
 @Component({
-    template :`
-        <h1>Users</h1>
-    `
+    templateUrl : 'app/users.component.html',
+    providers :[UserService]
 })
-export class UsersComponent {
-
+export class UsersComponent implements OnInit {
+    users : any[];
+    constructor(private _userService: UserService) {
+      
+    }
+    ngOnInit() {
+        this._userService.getUsers()
+            .subscribe(users => this.users = users);
+    }
 }
