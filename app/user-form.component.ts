aa6c8b7f5dbe9c89db8584e13d23e41059a86fbf
Vinay Joshi <vinay01joshi@gmail.com>
2017-07-18 14:39:41 +0530

    Create a user.

--- app/user-form.component.ts
+++ app/user-form.component.ts
@@ -1,25 +1,31 @@
 import { Component } from "angular2/core";
 import { ControlGroup, FormBuilder, Validators } from "angular2/common";
 import { BasicValidators } from "./basicValidators";
-import { CanDeactivate } from "angular2/router";
+import { CanDeactivate, Router } from "angular2/router";
+import { UserService } from "./user.service";
 
 @Component({
-    templateUrl : 'app/user-form.component.html'
+    templateUrl : 'app/user-form.component.html',
+    providers:[UserService]
 })
 export class UserFormComponent implements CanDeactivate{
     form: ControlGroup;
-    constructor(fb: FormBuilder){
-        this.form = fb.group({
+    constructor(
+        fb: FormBuilder,
+        private _userService : UserService, 
+        private _router : Router){
+       
+            this.form = fb.group({
             name : ['',Validators.required],
             email: ['',BasicValidators.email],
             phone:[],
             address : fb.group({
-                    street :[],
-                    suite :[],
-                    city :[],
-                    zipcode:[]
+               street :[],
+               suite :[],
+               city :[],
+               zipcode:[]
             })
-        })
+        });
     }
 
     routerCanDeactivate() {
@@ -27,5 +33,11 @@ export class UserFormComponent implements CanDeactivate{
             return confirm('You have unsaved changes. Are you want to navigate away?');
 
         return true;
+    }
+    save(){
+        this._userService.addUser(this.form.value)
+            .subscribe(x=> {
+                this._router.navigate(['Users']);
+            });
     }
 }
