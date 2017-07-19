c615222f399fdb693a94960aa26f7e8582c2f1de
Vinay Joshi <vinay01joshi@gmail.com>
2017-07-19 08:30:49 +0530

    Implement master/detail to view post details.

--- app/posts.component.ts
+++ app/posts.component.ts
@@ -5,17 +5,37 @@ import { SpinnerComponent } from "./spinner.component";
 
 @Component({
     templateUrl :'app/posts.component.html',
+    styles:[`
+    .posts li {
+        cursor : default;
+    }
+    .posts list:hover {
+        background : #ecf0f1;
+    }
+    .list-group-item.active,
+    .list-group.item.active:hover,
+    .list-group.item.active:focus {
+        background-color : #ecf0f1;
+        border-color : #ecf0f1;
+        color :#2c3e50;
+    }
+    `],
     providers : [PostService],
     directives : [SpinnerComponent]
 })
 export class PostsComponent  implements OnInit{
     posts : [any];
     isLoading= true;
+    currentPost;
     constructor(private _postService: PostService) {
 
    } 
     ngOnInit() {
         this._postService.getPosts()
             .subscribe(posts=> this.posts = posts,null,()=> {this.isLoading = false});
+    }
+
+    select(post) {
+        this.currentPost = post;
     }
 }
