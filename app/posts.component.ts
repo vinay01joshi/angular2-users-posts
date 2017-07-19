1e6c368396a2a52b01d5f2a25562631205d49bd5
Vinay Joshi <vinay01joshi@gmail.com>
2017-07-19 08:06:07 +0530

    Display the list of posts.

--- app/posts.component.ts
+++ app/posts.component.ts
@@ -1,9 +1,18 @@
 
-import { Component } from "angular2/core";
+import { Component, OnInit } from "angular2/core";
+import { PostService } from "./post.service";
 
 @Component({
-    template    :`<h1>Posts</h1>`
+    templateUrl :'app/posts.component.html',
+    providers : [PostService]
 })
-export class PostsComponent {
+export class PostsComponent  implements OnInit{
+    posts : [any];
+    constructor(private _postService: PostService) {
 
+   } 
+    ngOnInit() {
+        this._postService.getPosts()
+            .subscribe(posts=> this.posts = posts);
+    }
 }
